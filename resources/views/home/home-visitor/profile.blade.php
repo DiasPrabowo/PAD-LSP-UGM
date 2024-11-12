8dc50d12c56a70521daad3f25b68f73b0b4ebf45
Lintangs <diaslintangprabowo0@gmail.com>
2024-11-12 18:21:05 +0700

    profile page

--- resources/views/home/home-visitor/profile.blade.php
+++ resources/views/home/home-visitor/profile.blade.php
@@ -3,6 +3,10 @@
 @section('title', 'Home - Lembaga Sertifikasi Profesi UGM')
 
 @section('content')
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" />
+<script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"></script>
+
+
 <div class="flex justify-center items-center py-12 px-4 md:px-8 lg:px-12">
     <div class="bg-white w-full max-w-4xl p-8 rounded-lg shadow-lg">
         <h1 class="text-3xl font-bold text-gray-800 mb-6 border-b-2 border-gray-200 pb-3 text-center">LEGAL PENDIRIAN</h1>
@@ -19,9 +23,37 @@
         </ul>
     </div>
 </div>
+
 <div class="flex justify-center items-center py-12 px-4 md:px-8 lg:px-12">
-<div class="bg-white w-full max-w-4xl p-8 rounded-lg shadow-lg">
-<h1 class="text-3xl font-bold text-gray-800 mb-6 border-b-2 border-gray-200 pb-3 text-center">STRUKTUR ORGANISASI</h1>
+    <div class="bg-white w-full max-w-4xl p-8 rounded-lg shadow-lg">
+        <h1 class="text-3xl font-bold text-gray-800 mb-6 border-b-2 border-gray-200 pb-3 text-center">STRUKTUR ORGANISASI</h1>
+        <img src="{{ asset('images/Pengurus.png') }}" alt="Struktur Organisasi" class="w-full h-auto">
+    </div>
 </div>
+
+<div class="flex justify-center items-center py-12 px-4 md:px-8 lg:px-12">
+    <div class="bg-white w-full max-w-4xl p-8 rounded-lg shadow-lg">
+        <h1 class="text-3xl font-bold text-gray-800 mb-6 border-b-2 border-gray-200 pb-3 text-center">LOKASI</h1>
+        
+        <!-- Add the map container div here -->
+        <div id="map" style="width: 100%; height: 500px; border-radius: 8px;"></div>
+
+        <script>
+            var map = L.map('map', { attributionControl: false }).setView([-7.7749, 110.3748], 13);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+            }).addTo(map);
+
+            // Add a marker with a popup
+            var marker = L.marker([-7.7738306, 110.3724111]).addTo(map);
+            marker.bindPopup(
+                "<div style='text-align: center;'>" +
+                "<b><a href='https://maps.app.goo.gl/1MvvKeFbT7G71AJcA' target='_blank' style='text-decoration: none; color: inherit;'>Lembaga Sertifikasi Profesi <br> Universitas Gadjah Mada</a></b>" +
+                "</div>"
+            ).openPopup();
+        </script>
+    </div>
 </div>
+
 @endsection
